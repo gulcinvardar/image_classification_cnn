4a867e1d11a93686945b8e64fe584fbc5fea427a
gulcinvardar <100417081+gulcinvardar@users.noreply.github.com>
2022-07-01 17:47:26 +0200

    Modified the doc strings

--- utils_camera.py
+++ utils_camera.py
@@ -1,12 +1,9 @@
 import logging
-import pandas as pd
 import numpy as np
 from  matplotlib import pyplot as plt
 import os
 import cv2
-from tensorflow import keras
 from keras.preprocessing import image 
-from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.resnet50 import preprocess_input
 from utils_model import classes
 
@@ -26,7 +23,9 @@ def write_image(out_folder, frame, filename):
 
 
 def key_action():
-    """Taken from course material"""
+    """
+    Taken from course material
+    """
     # https://www.ascii-code.com/
     k = cv2.waitKey(1)
     if k == 113: # q button
@@ -40,18 +39,16 @@ def key_action():
 
 def init_cam(width, height):
     """
-    Setups and creates a connection to the webcam.
+    Sets  and creates a connection to the webcam.
     Taken from course material.
     """
 
     logging.info('start web cam')
     cap = cv2.VideoCapture(0)
 
-    # Check success
     if not cap.isOpened():
         raise ConnectionError("Could not open video device")
     
-    # Set properties. Each returns === True on success (i.e. correct resolution)
     assert cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     assert cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
@@ -59,8 +56,9 @@ def init_cam(width, height):
 
 
 def read_new_image(out_folder, filename):
-    """Reads the image saved by the camera."""
-    #time = datetime.now().strftime("%Y%m%d")
+    """
+    Reads the image saved by the camera.
+    """
     image_path = f'image-{str(filename)}.png'
     img = image.load_img(f'{out_folder}{image_path}', target_size=(128,128))
     img_array = image.img_to_array(img, dtype='uint8')
@@ -68,14 +66,18 @@ def read_new_image(out_folder, filename):
     return img, img_array
 
 def process_image(img_array):
-    """Preprocesses the newly acquired image for the model."""
+    """
+    Preprocesses the newly acquired image for the model.
+    """
     proc_img = preprocess_input(img_array)
     proc_img = np.expand_dims(proc_img, axis = 0)/255
 
     return proc_img
 
 def predict_image(proc_img, m):
-    """Predicts the image based on loaded model."""
+    """
+    Predicts the image based on loaded model.
+    """
     pred = m.predict(proc_img)
     max_value = max(pred)
     pred_index = np.argmax(max_value)
@@ -84,7 +86,9 @@ def predict_image(proc_img, m):
     return classify
 
 def write_on_img(img_array, classify, filename, window_name, out_folder):
-    """Writes the prediction on the image and saves it."""
+    """
+    Writes the prediction on the image and saves it.
+    """
     cv2.putText(
         img= img_array, text=classify, org= (0, 50),
         fontScale=1, color=(255, 0, 0), 
--- utils_model.py
+++ utils_model.py
@@ -11,8 +11,10 @@ from tensorflow.keras.applications.resnet50 import preprocess_input
 from  matplotlib import pyplot as plt
 
 
-def class_names(train_datagen):
-    """Retrieves the class names based on the folder names"""
+def class_names(train_datagen, base_path):
+    """
+    Retrieves the class names based on the folder names
+    """
     all_data = train_datagen.flow_from_directory(
     directory=base_path,
     class_mode="input",
@@ -22,7 +24,9 @@ def class_names(train_datagen):
     return classes
 
 def image_generator():
-    """Opens and preprocesses the data-set"""
+    """
+    Opens and preprocesses the data-set
+    """
     train_datagen = image.ImageDataGenerator(
     preprocessing_function=preprocess_input,
     rescale=1.0/255.0,
@@ -32,8 +36,10 @@ def image_generator():
     return train_datagen
 
 
-def train_val_sets(train_datagen):
-    """Creates the train and test data-sets"""
+def train_val_sets(train_datagen, base_path):
+    """
+    Creates the train and test data-sets. 
+    """
     train_generator = train_datagen.flow_from_directory(
     directory=base_path,
     target_size=target_size, 
@@ -52,8 +58,11 @@ def train_val_sets(train_datagen):
     
     return train_generator, validation_generator
 
-def images_for_pred():
-    """To predict the images in the test-folder"""
+def images_for_pred(test_path):
+    """
+    To predict the images in the test-folder. 
+    Further evaluation of the model.
+    """
     pred_batches = image.ImageDataGenerator(
     preprocessing_function=preprocess_input, 
     rescale=1.0/255.0
@@ -72,7 +81,7 @@ input_shape = (128, 128, 3)
 epochs = 150
 batch_size=32
 
-train_datagen = image_generator()
-classes = class_names(train_datagen)
-train_generator, validation_generator = train_val_sets(train_datagen)
+train_datagen = image_generator(test_path)
+classes = class_names(train_datagen, base_path)
+train_generator, validation_generator = train_val_sets(train_datagen, base_path)
 
